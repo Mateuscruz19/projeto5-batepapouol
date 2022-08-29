d289c776b33c1076c081df0092d1e554db693a6a
Mateuscruz19 <fdsmeuirmao2@outlook.com>
2022-08-29 11:26:58 -0300

    Requisitos Prontos

--- js/livechat.js
+++ js/livechat.js
@@ -1,20 +1,25 @@
-// VARIAVEIS GLOBAIS
+ // BONUS: BUSCAR LISTA DE PARTICIPANTES;
+ // VARIAVEIS GLOBAIS
 let NickName;
 let membros = [];
+let mensagensdochat = [];
+let mensagemdigitada = '';
+let mensagemformatada='';
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.menssager');
 
 // Perguntar nome de usuario
 const pedirnome = () => {
     NickName = prompt('Ola usuario,voce precisa se identificar para entrar,qual o seu NickName?');
 
 }
-
 pedirnome();
 
 // Nickname formatado do usuario
 const nickformatado = {
     name:`${NickName}`
 }
-
 
 //  se o servidor enviar a resposta
 const enviarmembro = (resposta) => {
@@ -34,7 +39,7 @@ const membroerro = (erro) => {
 
 
 const deutudocerto = () => {
-    console.log('Seu nickname foi atualizado'); 
+    
 }
 
 const deutudoerrado = (err) => {
@@ -53,6 +58,7 @@ function atualizandochat() {
     promessaatt.catch(erratt); // erro
 }
 
+
 setInterval(atualizandochat, 5000);
 
 const erratt = (er) => {
@@ -64,9 +70,84 @@ const atualizar = (re) => {
     
 }
 
+const popularMensagens = (mensagens) => {
+    let ListaMensagens = mensagens.data;
+    let ul = document.querySelector('.conteiner');
+    let carregar = '';
+    for(i = 0; i < ListaMensagens.length;i++){
+        if(ListaMensagens[i].type=="status"){
+            ul.innerHTML = carregar += `<li>
+            <div class="mensagem entrou">
+                <h1><span class="horario">${ListaMensagens[i].time} </span><span class="usuario">${ListaMensagens[i].from} </span>${ListaMensagens[i].text} </h1>
+            </div>
+        </li>`
+        }else if(ListaMensagens[i].type=="message"){
+            ul.innerHTML = carregar += `<li>
+            <div class="mensagem normal">
+                <h1><span class="horario">${ListaMensagens[i].time} </span><span class="usuario">${ListaMensagens[i].from} </span>para <span class="usuario">${ListaMensagens[i].to}</span> ${ ListaMensagens[i].text } </h1>
+            </div>
+        </li>`
+        }else if (ListaMensagens[i].to === NickName){
+            ul.innerHTML.private_message = carregar += `<li>
+            <div class="mensagem reservado">
+                <h1><span class="horario">${ListaMensagens[i].time} </span><sp an class="usuario">${ListaMensagens[i].from} </span> reservadamente para <span class="usuario">${ListaMensagens[i].to} </span> ${ListaMensagens[i].text}</h1>
+            </div>
+        </li>`
+        }
+}
+}
 
 
 
+function recebermensagemErro(erroo) {
+    alert('Puts,erro ao receber mensagem');
+    console.log(erroo);
+}
+
+// BONUS: BUSCAR LISTA DE PARTICIPANTES;
 const promessa = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants') 
 promessa.then(enviarmembro); // sucesso
-promessa.catch(membroerro);
+promessa.catch(membroerro);  // Error
+ 
+// FUNÇÃ
+const scrollarmensagens = () => {
+    let ultimamensagem = document.querySelector('.conteiner').lastElementChild;
+    ultimamensagem.scrollIntoView( {block: "end", inline: "nearest"});
+}
+
+setInterval(scrollarmensagens, 1000);
+
+
+
+// BUSCAR MENSAGENS DO CHAT
+
+function ResetarMensagens() {
+    const promessachat = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    promessachat.then(popularMensagens); // sucesso
+    promessachat.catch(recebermensagemErro); // erro
+
+
+}
+
+setInterval(ResetarMensagens, 3000);
+
+
+
+function recebermensagem() {
+    mensagemdigitada = input.value;
+    mensagemformatada = {
+    from: NickName,
+    to: "Todos",
+    text: mensagemdigitada,
+    type: "message"
+};
+    const promessam = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', mensagemformatada);
+    promessam.then(deucertoooo); // sucesso
+    promessam.catch(recebermensagemErro); // erro
+    console.log('eu sou a variavel mensagem formatada: ', mensagemformatada);
+}
+
+
+function deucertoooo() {
+    input.value = "";
+}
